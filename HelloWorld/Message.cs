b7c2573f2aca11390cb894a5163be3164b50b6a0
Ser-Custodio <sergio.custodio@gmail.com>
2018-08-20 15:37:47 +0200

    first commit

---
+++ HelloWorld/Message.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Message
+    {
+        protected int matin = 9;
+        protected int midi = 13;
+        protected int soir = 18;
+
+        public String GetHelloMessage ()
+        {
+            String message = "";
+
+            DateTime date = new DateTime(2018, 08, 20, 15, 10, 10);
+            // get the day of the week according to the date
+            DayOfWeek day = date.DayOfWeek;
+
+            // Change the message according to date and time
+            // if its between friday 18h and Monday before 9h
+            if (date.DayOfWeek == DayOfWeek.Friday && date.Hour > this.soir || date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Monday && date.Hour < this.matin)
+            {
+                message = "Bon week-end";
+            }
+            // if it's between 18h and 9h
+            else if (date.Hour >= this.soir || date.Hour < this.matin)
+            {
+                message = "Bonsoir";
+            }
+            // if it's between 13h and 18h
+            else if (date.Hour >= this.midi)
+            {
+                message = "Bon après-midi";
+            }
+            // if it's between 9h and 13h
+            else if (date.Hour >= this.matin)
+            {
+                message = "Bonjour";
+            }
+            return message;
+        }
+        public Message(int matin, int midi, int soir)
+        {
+            this.matin = matin;
+            this.midi = midi;
+            this.soir = soir;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String exit = "";
+            do
+            {
+                Message message = new Message(9, 13, 18);
+                System.Console.WriteLine(message.GetHelloMessage() + " " + Environment.UserName);
+                exit = System.Console.ReadLine();
+            } while (exit != "exit");
+        }
+    }
+}
